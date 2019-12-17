e99de766291c89ba1ff447e4c4f198c61ee5a582
Colin Turing <colin.koga@gmail.com>
2019-12-17 16:51:02 -0700

    Make App a class component

--- rancid-tomatillos/src/containers/App/App.js
+++ rancid-tomatillos/src/containers/App/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from '../../Components/Header/Header';
 import { Route } from 'react-router-dom';
 
 
-const App = () => {
-  return (
-    <div className='app-container'>
-      <Route exact path='/' render={() => {
-        <Header />
-      }}
-      />
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className='app-container'>
+        <Route exact path='/' render={() => {
+          return <Header />
+        }}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
