f23e5b724d208a8e5effabbe8505693a13fb721a
Colin Turing <colin.koga@gmail.com>
2019-12-17 19:34:50 -0700

    Add mapStateToProps to MovieContainer recieving error

--- rancid-tomatillos/src/containers/App/App.js
+++ rancid-tomatillos/src/containers/App/App.js
@@ -8,35 +8,49 @@ import { connect } from 'react-redux';
 
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
   }
 
   componentDidMount = () => {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
       .then(res => res.json())
-      .then(data => this.props.addMovies(data))
+      .then(data => {
+        console.log(data)
+        console.log(this.props.addMovies)
+        this.props.addMovies(data)
+        this.setState({ isLoading: false })
+      })
+      
       .catch(err => console.log(err))
   } 
 
   render() {
+    const { isLoading } = this.state
     return (
       <div className='app-container'>
-        <Route path='/' render={() => {
-          return <Header />
-        }}
-        />
-        <Route exact path='/' render={() => {
-          return <MoviesContainer />
-        }}
-        />
+        {isLoading ? <h2>Loading...</h2> 
+            : <>
+                <Route path='/' render={() => {
+                return <Header />
+                }}
+                />
+                <Route path='/' render={() => {
+                return <MoviesContainer />
+                }}
+                />
+              </>
+        }
       </div>
     )
   }
-}
+};
 
 const mapDispatchToProps = dispatch => ({
   addMovies: movies => dispatch( addMovies(movies) )
-})
+});
 
 export default connect(null, mapDispatchToProps)(App);
--- rancid-tomatillos/src/containers/MoviesContainer/MoviesContainer.js
+++ rancid-tomatillos/src/containers/MoviesContainer/MoviesContainer.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import MovieCard from '../../Components/MovieCard/MovieCard';
+import { connect } from 'react-redux';
 import './MoviesContainer.scss';
+import { render } from 'react-dom';
 
-const MoviesContainer = () => {
+const MoviesContainer = ({data}) => {
+  console.log(data)
+  const moviesList = data.movies.map(movie => {
+    return (
+      <MovieCard 
+        key={movie.id}
+        id={movie.id}
+        poster={movie.poster_path}
+        avgRating={movie.average_rating}
+      />
+    )
+  })
+  console.log(moviesList)
+
   return (
-    <main className='main-movies-container'></main>
+    <main className='main-movies-container'>
+      {moviesList}
+    </main>
   )
-};
+}
 
-export default MoviesContainer;
+const mapStateToProps = state => ({
+  data: state.movies
+});
+
+export default connect(mapStateToProps)(MoviesContainer);
