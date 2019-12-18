9fb5c6501b439807d98520fbca779395d24c8cb3
John Adams <adamsjr8576@gmail.com>
2019-12-17 23:52:08 -0700

    Add images and export import them into cards to display based on rating

--- rancid-tomatillos/src/Components/MovieCard/MovieCard.js
+++ rancid-tomatillos/src/Components/MovieCard/MovieCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import './MovieCard.scss';
+import images from '../../images/images'
 
 const MovieCard = ({ id, poster, avgRating }) => {
+  let icon;
+  if (avgRating < 4) {
+    icon = images.badTomatillo;
+  } else if (avgRating > 3 && avgRating < 7) {
+    icon = images.okayTomatillo;
+  } else if (avgRating > 6) {
+    icon = images.goodTomatillo;
+  }
+    console.log(icon)
   return (
     <article data-id={id} className='movie-card'>
-      <img src={poster} alt='movie poster image' className='movie-card-img' />
+      <section className='section-movie-image' style={{backgroundImage: `url(${poster})`}}>
+      </section>
       <section className='section-avg-rating'>
         <p className='average-rating-p'>Average Rating</p>
-        <p className='average-rating-num'>{avgRating}</p>
+        <section className='rating-section'>
+          <img src={icon} alt='image of rancid tomatillo' className='tomatillo-icon' />
+          <p className='average-rating-num'>{avgRating}</p>
+        </section>
       </section>
     </article>
   )
