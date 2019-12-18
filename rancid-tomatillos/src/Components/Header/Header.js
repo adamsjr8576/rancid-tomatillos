03d5e3e67d1375ddc037ed7cff0907246d9bf577
James Miller <james.rex.miller.4@gmail.com>
2019-12-18 16:59:34 -0700

    Add conditional rendering login/logout buttons

--- rancid-tomatillos/src/Components/Header/Header.js
+++ rancid-tomatillos/src/Components/Header/Header.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './Header.scss';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { removeUser, updateLoggedIn } from '../../actions/index';
 
 
-const Header = ({ data }) => {
+const Header = ({ data, isLoggedIn, removeUser, updateLoggedIn }) => {
+  let log;
+  if (isLoggedIn) {
+    log = <button onClick={() => {updateLoggedIn(isLoggedIn); removeUser();}} className='header-btn'>Log Out</button>
+  } else {
+    log = <Link to='/login' className='login-link'>
+    <button className='header-btn'>Log In</button>
+    </Link>
+  }
   const index = Math.floor(Math.random() * Math.floor(20));
   return (
     <header className='header-main' style={{backgroundImage: `url(${data.movies[index].backdrop_path})`}}>
       <h1 className='header-h1'>Rancid Tomatillos</h1>
       <section className='header-btn-section'>
-        <Link to='/login' className='login-link'>
-        <button className='header-btn'>Log In</button>
-        </Link>
+        {log}
         <button className='header-btn'>Home</button>
       </section>
     </header>
