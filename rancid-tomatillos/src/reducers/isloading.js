2460ac58b9d9740332017b6553657314ca82be64
John Adams <adamsjr8576@gmail.com>
2019-12-31 08:53:23 -0500

    Add isLoading to redux store and implement updating functionality on componentDidMount in App

--- rancid-tomatillos/src/reducers/isloading.js
+++ rancid-tomatillos/src/reducers/isloading.js
@@ -2,6 +2,7 @@ export const isLoadingReducer = (state = true, action) => {
   switch (action.type) {
     case 'UPDATE_IS_LOADING' :
       return action.isLoading
-    default:
+    default :
+      return state
   }
 }
