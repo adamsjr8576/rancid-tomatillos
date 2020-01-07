015e2a7924ba60548f1470ef270d0a63f07fe497
John Adams <adamsjr8576@gmail.com>
2020-01-07 13:54:21 -0700

    Add film background image for header when on moviepage

--- src/images/images.js
+++ src/images/images.js
@@ -1,11 +1,13 @@
 import goodTomatillo from './goodTomatillo.png';
 import okayTomatillo from './okayTomatillo.png';
 import badTomatillo from './badTomatillo.png';
+import film from './film.png';
 
 const images = {
   goodTomatillo: goodTomatillo,
   okayTomatillo: okayTomatillo,
-  badTomatillo: badTomatillo
+  badTomatillo: badTomatillo,
+  filmImage: film
 }
 
 export default images;
